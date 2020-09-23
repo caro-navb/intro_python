99c79828983690f4bf2c8315e1c0c8911b797b7d
Carolina Navarro <silvia.navarro7499@alumnos.udg.mx>
2020-09-22 20:41:56 -0500

    Hice un script que le pida al usuario su dia y mes de nacimiento, y le diga que signo zodiacal es

---
+++ signo.py
@@ -0,0 +1,39 @@
+dia = int(input("Ingresa tu dia de nacimiento: "))
+mes = int(input("Ingresa tu mes de nacimiento: "))
+
+if (mes == 3 and dia >= 21) or (mes == 4 and dia <= 20):
+    print("Tu signo zodiacal es Aries")
+
+elif (mes == 4 and dia >= 21) or (mes == 5 and dia <= 21):
+    print("Tu signo zodiacal es Tauro")
+
+elif (mes == 5 and dia >= 5) or (mes == 6 and dia <= 21):
+    print("Tu signo zodiacal es Géminis")
+
+elif (mes == 6 and dia >= 21) or (mes == 7 and dia <= 23):
+    print("Tu signo zodiacal es Cáncer")
+
+elif (mes == 7 and dia >= 24) or (mes == 8 and dia <= 23):
+    print("Tu signo zodiacal es Leo")
+
+elif (mes == 8 and dia >= 24) or (mes == 9 and dia <= 23):
+    print("Tu signo zodiacal es Virgo")
+
+elif (mes == 9 and dia >= 24) or (mes == 10 and dia <= 23):
+    print("Tu signo zodiacal es Libra")
+
+elif (mes == 10 and dia >= 24) or (mes == 11 and dia <= 22):
+    print("Tu signo zodiacal es Escorpio")
+
+elif (mes == 11 and dia >= 23) or (mes == 12 and dia <= 21):
+    print("Tu signo zodiacal es Sagitario")
+
+elif (mes == 12 and dia >= 22) or (mes == 1 and dia <= 20):
+    print("Tu signo zodiacal es Capricornio")
+
+elif (mes == 1 and dia >= 21) or (mes == 2 and dia <= 19):
+    print("Tu signo zodiacal es Acuario")
+
+elif (mes == 2 and dia >= 20) or (mes == 3 and dia <= 20):
+    print("Tu signo zodiacal es Piscis")
+
